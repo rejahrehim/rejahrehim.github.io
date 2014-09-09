18dc668440432bb93474f0813a89ab784f28ee50
Rejah Rehim <rejah@digitalbrandgroup.com>
2014-09-09 11:05:25 +0530

    Adding new Slide for FFOS Hackday

---
+++ slides/files/fxos/fxos.js
@@ -0,0 +1,3 @@
+function printLogo(){
+	document.write('<img class="ffos" style="margin:0px 0 -25px 0; padding-top:0px; border:0px; box-shadow:none;" height="70" src="files/fxos/ffos.png" style="border: 0; box-shadow:none;">');
+}
